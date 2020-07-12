cb57777348ed9e220feff7d958428e57f295c991
Patel Krushankant <patelkk1618@gmail.com>
2020-07-12 15:31:19 -0500

    Initial commit

---
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {AngularFireAuth} from "@angular/fire/auth";
+import * as firebase from "firebase";
+
+@Component({
+  selector: 'bs-navbar',
+  templateUrl: './bs-navbar.component.html',
+  styleUrls: ['./bs-navbar.component.css']
+})
+export class BsNavbarComponent implements OnInit {
+
+  user: firebase.User;
+  constructor(private afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(user=> this.user = user);
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  logout() {
+    this.afAuth.signOut().then(r => {
+    });
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyB17BcVb_WynmSyRpnbSoTvZdPh706xq40",
+    authDomain: "naturalshop-4316b.firebaseapp.com",
+    databaseURL: "https://naturalshop-4316b.firebaseio.com",
+    projectId: "naturalshop-4316b",
+    storageBucket: "naturalshop-4316b.appspot.com",
+    messagingSenderId: "180462389565",
+    appId: "1:180462389565:web:674c4afb1e0138cecb9d54",
+    measurementId: "G-70RN1KTC4R"
+  }
+};
