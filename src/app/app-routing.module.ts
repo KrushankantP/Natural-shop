fce100a8f6f8ea0ca2a34f7be49e788d35e91b7e
Patel Krushankant <patelkk1618@gmail.com>
2020-07-13 04:11:16 -0500

    Redirect the users after logging in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,13 +14,13 @@ import {AuthGuard} from "./auth-gurd.service";
 
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'products', component: ProductsComponent},
-  {path: 'shopping-cart', component: ShoppingCartComponent},
+  { path: '', component: HomeComponent},
+  { path: 'products', component: ProductsComponent},
+  { path: 'shopping-cart', component: ShoppingCartComponent},
   { path: 'check-out', component: CheckOutComponent, canActivate: [AuthGuard] },
   { path: 'order-success', component: OrderSuccessComponent, canActivate: [AuthGuard] },
   { path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGuard] },
-  {path: 'login', component:LoginComponent},
+  { path: 'login', component:LoginComponent},
   { path: 'admin/products', component: AdminProductsComponent, canActivate: [AuthGuard] },
   { path: 'admin/orders', component: AdminOrdersComponent, canActivate: [AuthGuard] }
 ];
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Observable} from "rxjs";
 import {AngularFireAuth} from "@angular/fire/auth";
+import { ActivatedRoute } from '@angular/router';
 import * as firebase from "firebase";
 
 @Injectable({
@@ -9,12 +10,14 @@ import * as firebase from "firebase";
 export class AuthService {
 
   user$ : Observable<firebase.User>;
-  constructor(private _afAuth: AngularFireAuth) {
+  constructor(private _afAuth: AngularFireAuth,
+              private _route: ActivatedRoute) {
     this.user$ = _afAuth.authState;
   }
 
   login(){
-    this._afAuth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+    let returnUrl = this._route.snapshot.queryParamMap.get('returnUrl') || '/';
+    localStorage.setItem('returnUrl', returnUrl);
   }
   logout(){
     this._afAuth.signOut();
