f88ff1589a628415fc025a5a1f959ac879b0acea
Patel Krushankant <patelkk1618@gmail.com>
2020-07-17 16:58:11 -0500

    order success and order detail view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
 import {ProductsComponent} from "./products/products.component";
 import {ShoppingCartComponent} from "./shopping-cart/shopping-cart.component";
 import {OrderSuccessComponent} from "./order-success/order-success.component";
@@ -13,6 +12,7 @@ import {MyOrdersComponent} from "./my-orders/my-orders.component";
 import {AuthGuard} from "./auth-gurd.service";
 import {AdminAuthGuardService} from "./admin-auth-guard.service";
 import {ProductFormComponent} from "./admin/product-form/product-form.component";
+import {MyOrdersDetailComponent} from "./my-orders/my-orders-detail/my-orders-detail.component";
 
 
 const routes: Routes = [
@@ -22,6 +22,7 @@ const routes: Routes = [
   { path: 'check-out', component: CheckOutComponent, canActivate: [AuthGuard] },
   { path: 'order-success', component: OrderSuccessComponent, canActivate: [AuthGuard] },
   { path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGuard] },
+  {path: 'my/orders/:id', component: MyOrdersDetailComponent, canActivate: [AuthGuard]},
   { path: 'login', component:LoginComponent},
 
   { path: 'admin/products/new',
--- src/app/order-success/order-success.component.ts
+++ src/app/order-success/order-success.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-order-success',
+  selector: 'order-success',
   templateUrl: './order-success.component.html',
   styleUrls: ['./order-success.component.css']
 })
-export class OrderSuccessComponent implements OnInit {
+export class OrderSuccessComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(_route: Router
+  ){
+    setTimeout(() => {
+      _route.navigate(['/'])
+    }, 5000 )
   }
 
 }
