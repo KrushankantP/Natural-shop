cd6d71d17cbbb730d1f37f4128641177c3088637
Patel Krushankant <patelkk1618@gmail.com>
2020-07-27 15:54:49 -0500

    NGX-DataTAble Added in Admin Product

--- src/app/admin/components/admin-products/admin-products.component.ts
+++ src/app/admin/components/admin-products/admin-products.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {ProductService} from "../../../shared/services/product.service"
-import {IProduct} from "../../../shared/models/IProduct";
-import { Subscription } from "rxjs";
+import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {ProductService} from '../../../shared/services/product.service';
+import {IProduct} from '../../../shared/models/IProduct';
+import {Router} from "@angular/router";
 
 
 @Component({
@@ -9,32 +9,60 @@ import { Subscription } from "rxjs";
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit, OnDestroy {
+export class AdminProductsComponent implements OnInit {
 
+  @ViewChild('actionTemplate') actionTemplate: TemplateRef<any>;
   products: IProduct[];
-  subscription: Subscription;
-  filteredProducts: any[];
+  productColumn = [];
+  filteredProducts;
+  items;
 
-  constructor(private _productService: ProductService) {
 
-    this.subscription = _productService.getAllProducts()
-      .subscribe(products =>
-        (this.filteredProducts = this.products= products)
-      );
+  constructor(private _productService: ProductService,
+              private _router: Router ) {
+
   }
 
-  filter(query: string){
-    this.filteredProducts = query ?
-      this.products.filter(product =>
-      product.title.toLowerCase().includes(query.toLowerCase())
-      )
-      : this.products
+  updateFilter(event) {
+    const val = event.target.value.toLowerCase();
+
+    // filter our data
+    this.filteredProducts  = this.items.filter(data => {
+      return data.name.toLowerCase().indexOf(val) !== -1 || !val;
+    });
+
+    // update the rows
+    this.products = this.filteredProducts ;
+    // Whenever the filter changes, always go back to the first page
+    // this.myFilterTable.offset = 0;
   }
+
+  // filter(query: string){
+  //   this.filteredProducts = query.toLowerCase().trim();
+  //   this.products.filter(product =>
+  //     product.title.toLowerCase().includes(query.toLowerCase())
+  //   );
+  // }
   ngOnInit(): void {
+    this._productService.getAllProducts()
+      .subscribe(products => {
+        this.products = products;
+        this.productColumn = [
+          {prop: 'title', name: 'Title'},
+          {prop: 'category', name: 'Category'},
+          {prop: 'price', name: 'Price'},
+          {prop: 'key', name: 'Actions', cellTemplate : this.actionTemplate}
+        ];
+        console.log("this.products", this.products)
+      });
   }
 
+  // ngOnDestroy(): void {
+  //   this.subscription.unsubscribe();
+  // }
 
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+  onEdit(id) {
+    console.log("id", id)
+    this._router.navigate(['/admin/products', id]);
   }
 }
